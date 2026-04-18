a3d53cffc678440b5aad0271f73dffc9fad91e4d
SleepyPandas <tommyrobotics1@gmail.com>
2026-04-18 16:56:58 -0400

    Added UARTSample_t Struct for UART Queue

--- Core/Inc/app_freertos.h
+++ Core/Inc/app_freertos.h
@@ -58,6 +58,16 @@ typedef struct {
   float Accel_Z;
 } UIState_t;
 
+typedef struct {
+  int16_t Gyro_X;
+  int16_t Gyro_Y;
+  int16_t Gyro_Z;
+
+  float Accel_X;
+  float Accel_Y;
+  float Accel_Z;
+} UARTSample_t;
+
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
